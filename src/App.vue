1ed9fb17c8144867d0e9f8f71e74d9318599264f
Dave <DavidRockin@users.noreply.github.com>
2019-08-14 20:03:58 -0400

    toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <el-button @click="startHacking">Start</el-button>
-    </div>
+    <Toolbar />
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
-    }
+  import Toolbar from './components/template/Toolbar'
+
+  export default {
+    components: { Toolbar }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Helvetica, sans-serif;
-  text-align: center;
-}
+  html, body {
+    padding: 0;
+    margin: 0;
+    background: #f1f1f1;
+  }
+  #app {
+    font-family: Helvetica, sans-serif;
+    text-align: center;
+  }
 </style>
---
+++ src/components/template/Toolbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-header style="padding: 0">
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+            <el-menu-item>
+                <i class="el-icon-folder"></i> Load Goal
+            </el-menu-item>
+            <el-menu-item>
+                <i class="el-icon-share"></i> Export
+            </el-menu-item>
+            <el-menu-item>
+                <i class="el-icon-plus"></i> Create Goal
+            </el-menu-item>
+            <el-menu-item style="float:right" @click="newTab('https://github.com/DavidRockin/reAction')">
+                <i class="el-icon-monitor"></i> Github
+            </el-menu-item>
+            <el-menu-item style="float:right" @click="newTab('https://volmit.com')">
+                <i class="el-icon-info"></i> VolmitSoftware
+            </el-menu-item>
+        </el-menu>
+    </el-header>
+</template>
+
+<script>
+  export default {
+    name: 'Toolbar',
+    methods: {
+      newTab(href) {
+        window.open(href, '_blank');
+      }
+    }
+  }
+</script>
