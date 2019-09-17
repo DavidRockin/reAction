28a5e1cd41e92e1a55bfa652a24642c60c566a83
Dave <DavidRockin@users.noreply.github.com>
2019-09-17 16:07:53 -0400

    switch to the new goal upon creation

--- src/models/Editor.js
+++ src/models/Editor.js
@@ -11,6 +11,14 @@ class EditorModel
         ReactStore.commit('newGoal', data || {
             name: `Untitle Goal`
         })
+        this.useLastGoal()
+    }
+
+    useLastGoal()
+    {
+        var id, g = Object.keys(ReactStore.state.goals)
+        if (g !== null && g.length !== 0 && (id = g.length - 1) >= 0)
+            this.openGoal(g[id], id)
     }
 
     openGoal(goal, id)
