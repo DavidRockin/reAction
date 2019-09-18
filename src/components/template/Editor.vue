36af33a518f17dbba4818d451be687ec6399575f
Dave <DavidRockin@users.noreply.github.com>
2019-09-18 10:27:52 -0400

    Update Editor.vue

--- src/components/template/Editor.vue
+++ src/components/template/Editor.vue
@@ -12,11 +12,44 @@
                     <el-form-item label="Name">
                         <el-input v-model="formLabelAlign.name"></el-input>
                     </el-form-item>
-                    <el-form-item label="Activity zone">
-                        <el-input v-model="formLabelAlign.region"></el-input>
+                    <el-form-item label="Description">
+                        <el-input v-model="formLabelAlign.description"></el-input>
                     </el-form-item>
-                    <el-form-item label="Activity form">
-                        <el-input v-model="formLabelAlign.type"></el-input>
+                    <el-form-item label="Author">
+                        <el-input v-model="formLabelAlign.author"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Conditions">
+                        <el-input v-model="formLabelAlign.conditions"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Reactions">
+                        <el-input v-model="formLabelAlign.reactions"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Interval">
+                        <el-input v-model="intervalAmount"></el-input>
+                        <el-select v-model="intervalType">
+                            <el-option
+                                v-for="(v, k) in intervalAmounts"
+                                :key="k"
+                                :label="v"
+                                :value="k"
+                            />
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="Max Health">
+                        <el-input v-model="formLabelAlign['action-health-max']"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Regen Health">
+                        <el-input v-model="formLabelAlign['action-health-regen']"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Damage Health">
+                        <el-input v-model="formLabelAlign['action-health-damage']"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Enabled">
+                        <el-switch
+                            v-model="formLabelAlign.enabled"
+                            active-color="#13ce66"
+                            inactive-color="#ff4949">
+                        </el-switch>
                     </el-form-item>
                 </el-form>
             </div>
@@ -31,26 +64,54 @@
 </style>
 
 <script>
+// infer from https://github.com/VolmitSoftware/React/blob/master/src/main/java/com/volmit/react/xrai/RAIGoal.java
+
 import { ReactStore } from '../../store/react'
 export default {
     data() {
         return {
             labelPosition: 'right',
+            intervalAmounts: {
+              t: 'Ticks',
+              s: 'Seconds',
+              m: 'Minutes',
+              h: 'Hours',
+              d: 'Days'
+            },
+            intervalAmount: 0,
+            intervalType: 's',
             formLabelAlign: {
-                name: '',
-                region: '',
-                type: ''
+                name: 'My test goal',
+                description: 'This will do some magic',
+                author: 'VolmitSoftware Studio',
+                enabled: true,
+                conditions: null,
+                reactions: null,
+                'action-health-max': '',
+                'action-health-regen': '',
+                'action-health-damage': ''
             }
+        }
+    },
+    methods: {
+        updateGoal (t) {
+            t = typeof t === 'object' ? t : this.formLabelAlign
+            t.interval = this.interval
+            ReactStore.state.activeGoal = t || this.formLabelAlign
+        }
+    },
+    computed: {
+        interval: function() {
+            return this.intervalAmount + this.intervalType
         }
     },
     watch: {
         formLabelAlign: {
-            handler: function (t, f) {
-                console.log(t)
-                ReactStore.state.activeGoal = t
-            },
+            handler: 'updateGoal',
             deep: true,
-        }
+        },
+        intervalAmount: 'updateGoal',
+        intervalType: 'updateGoal'
     }
 }
 </script>
